6d3283dcda09b0623fb3880034a8d286174d9c2b
faisalburhanudin <faisalburhanudin@gmail.com>
2022-07-23 21:43:21 +0700

    detail page

--- pages/post/index.tsx
+++ pages/post/index.tsx
@@ -1,8 +1,25 @@
 import {NextPage} from "next";
+import {Heading, List, ListItem, Text} from "@chakra-ui/react";
+import {Link} from '../../components/link';
 
 const PostIndex: NextPage = () => {
     return (
-        <h1>this is post index</h1>
+        <>
+            <Heading as="h1" mb={5}>Posts</Heading>
+            <List spacing={8}>
+                <ListItem>
+                    <Heading as="h2" size="md">
+                        <Link href="/post/hello-world" name="Create Polymorphic Inheritance in SQLAlchemy"/>
+                    </Heading>
+                    <Text fontSize="sm" mb={5}>
+                        18 April 2022
+                    </Text>
+                    <Text>
+                        How to make this
+                    </Text>
+                </ListItem>
+            </List>
+        </>
     )
 }
 
