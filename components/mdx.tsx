cc3fdf6ba3ca8f4edbc4321dfb694e5c7398f573
faisalburhanudin <faisalburhanudin@gmail.com>
2022-07-25 15:16:20 +0700

    homepage

--- components/mdx.tsx
+++ components/mdx.tsx
@@ -1,5 +1,10 @@
-import {Heading} from "@chakra-ui/react";
+import {Heading, Link, ListItem, Text, UnorderedList} from "@chakra-ui/react";
 
 export const MDXComponents: any = {
     h1: (p: any) => <Heading as="h1" my={5} {...p}/>,
+    h2: (p: any) => <Heading as="h2" my={5} {...p}/>,
+    ul: (p: any) => <UnorderedList {...p}/>,
+    li: (p: any) => <ListItem {...p}/>,
+    p: (p: any) => <Text mb={5} {...p}/>,
+    a: (p: any) => <Link color='teal.500' {...p}/>,
 }
